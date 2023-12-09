e5c33b96db8cc10d437def4198096153a93453d3
faizalkhan24 <kfaizal307@gmail.com>
2023-12-09 11:03:58 +0530

    categories complete static on home page day2

--- src/componets/programs/program.jsx
+++ src/componets/programs/program.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './program.css'
-
+import {programsData} from '../../data/programsData';
+import RightArrow from '../../assets/rightArrow.png'
 const program = () => {
   return (
     <div className="program" id="program">
@@ -8,6 +9,19 @@ const program = () => {
             <span className="stroke-text">Explore Our</span>
             <span>Programs</span>
             <span className="stroke-text">to shape you</span>
+        </div>
+        <div className="program-categories">
+            {programsData.map((program)=>(
+                <div className="categories">
+                    {program.image}
+                    <span>{program.heading}</span>
+                    <span>{program.details}</span>
+                    <div className="join-now">
+                        <span>Join Now</span>
+                        <img src={RightArrow} alt="" srcset="" />
+                    </div>
+                </div>
+            ))}
         </div>
     </div>
     )
